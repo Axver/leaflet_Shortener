f186b62a803200aacab987a4e1a06aaeced638c2
unknown <ecimoramora@gmail.com>
2019-05-31 13:46:58 +0700

    Leaflet Shortener

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="leaflet_Shortener.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
+    <style>
+    #mapid { height: 480px; }
+    </style>
+    <title>Jquery Plugin</title>
+</head>
+
+<body>
+
+    <a href="#">Test Link</a>
+    <a href="#">Test Link</a>
+
+    <div id="mapid"></div>
+
+    <script>
+        var token = 'pk.eyJ1IjoiYXh2ZXI3IiwiYSI6ImNqZjZ0NXk2NjA4NzI0MG44djVyOXU2cXAifQ.N-pJV3Uw0nOhjvLz9E4Zuw';
+        // Adding Maps
+        maps=$("#mapid").addMaps('mapid', token);
+        // Change Maps Center
+        maps.center(99.770569,0.215825);
+        // Adding Marker
+        marker=maps.marker(99.770569,0.215825);
+        // Adding Marker Colors
+        marker.color('blue');
+        marker.popup("<b>Test Popup</b>");
+        marker_1=maps.marker(99.780569,0.215825);
+        marker_1.color('red');
+        marker_1=maps.marker(99.780569,0.226825);
+        marker_1.color('circle_green');
+        marker_1.popup("text","open");
+        // Adding Click Function
+        maps.mapFunction("click");
+        // Disbaled Drag Function
+        maps.mapFunction("disabled_drag");
+        maps.mapFunction("locate",16);
+    </script>
+
+</body>
+
+</html>
---
+++ leaflet_Shortener.js
@@ -0,0 +1,78 @@
+$.fn.greenify = function () {
+    this.css("color", "green");
+};
+
+(function ($) {
+    var mymap;
+    $.fn.addMaps = function (id, token) {
+        mymap = L.map(id).setView([0.215825, 99.770569], 13);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: token
+        }).addTo(mymap);
+        var self = this;
+        this.center = function (lat, lon) {
+            mymap.panTo(new L.LatLng(lon, lat));
+            return self;
+        };
+        this.marker = function (lat, lon) {
+            var marker = L.marker([lon, lat]).addTo(mymap);
+            $.fn.color = function (color) {
+                if (color == 'blue') {
+                    iconUrl = 'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/66-512.png';
+                }
+                else if (color == 'red') {
+                    iconUrl = 'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/67-512.png';
+                }
+                else if (color == 'circle_green') {
+                    iconUrl = 'https://cdn3.iconfinder.com/data/icons/web-ui-color/128/Marker_green-512.png';
+                }
+                else if (color == 'circle_red') {
+                    iconUrl = 'https://cdn1.iconfinder.com/data/icons/ui-navigation-1/152/marker-512.png';
+                }
+                var myIcon = L.icon({
+                    iconUrl: iconUrl,
+                    iconSize: [80, 80]
+                });
+                marker.setIcon(myIcon);
+            }
+            // Popup
+            $.fn.popup = function (text, status) {
+                if (status == "open") {
+                    marker.bindPopup(text).openPopup();
+                }
+                else {
+                    marker.bindPopup(text);
+                }
+            }
+            var data;
+            $.fn.mapFunction = function (data,property) {
+                console.log(data);
+                if (data == 'click') {
+                    function onMapClick(e) {
+                        alert("You clicked the map at " + e.latlng);
+                    }
+                    mymap.on('click', onMapClick);
+                }
+                else if (data == 'disabled_drag') {
+                    mymap.dragging.disable();
+                }
+                else if (data == 'locate') {
+                    console.log("test");
+                    mymap.locate({ setView: true, maxZoom: 20 });
+                    if(property==null)
+                    {
+                        property=10;
+                    }
+                    mymap.setZoom(property);
+                    
+                }
+            }
+            return self;
+        }
+        return this;
+    };
+}(jQuery));
+
